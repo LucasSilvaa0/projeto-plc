1882189a0eae30505c9bbf22a8352c7274ddb882
Thomaz Cabral Correa de Araujo <tcca@WINDOWS.CIN.UFPE.BR>
2025-07-31 11:58:14 -0300

    correção de linguagem e valor

--- projeto/projeto.hs
+++ projeto/projeto.hs
@@ -3,26 +3,41 @@ type Numero = Double
 
 data Definicao = Def Id Termo
 
-data Termo = Var Id
-           | Lit Numero 
-           | Som Termo Termo
-           | Mul Termo Termo
-           | Lam [Id] Termo
-           | Apl Termo [Termo]
-           | Atr Id Termo
-           | Seq Termo Termo
+-- TODO: pode ser this também
+data LValue = LVar Id                      -- L-Value de uma variável simples, ex: "x"
+            | LAttr Expressao Id           -- L-Value de um atributo, ex: "x" em "p.x"
 
-           | If Termo Termo Termo 
-           | While Termo Termo
-           | For Termo Termo Termo Termo
+-- sempre produzem um valor. pode ser um LValue
+data Expressao = Lit Numero
+               | Var LValue
+               | Som Expressao Expressao
+               | Mul Expressao Expressao
 
-           | Classe Id [Id] [Definicao]
-           | New Id [Termo]
-           | GetAttr Id Termo
-           | SetAttr Termo Id Termo
-           | CallMethod Termo Id [Termo]
-           | InstanceOf Termo Id
+               | New Id -- não altera o estado, mas altera a heap
+               | InstanceOf Expressao Id
 
+               | Lam [Id] Comando
+               | CallMethod Expressao Id [Expressao]
+               | Apl Expressao [Expressao]
+
+-- muda o estado e controlam o fluxo
+data Comando = Atr LValue Expressao
+             | Seq Comando Comando
+             | If Expressao Comando Comando
+             | While Expressao Comando
+             | For Comando Expressao Comando Comando
+             | Classe Id [Id] [Definicao]
+             | CmdExpr Expressao
+
+type Endereco = Int
+
+data Valor = Num Numero                                      -- número
+           | ObjectVal Endereco                              -- ponteiro
+           | Fun ([Valor] -> Estado -> (Valor, Estado))      -- função. TODO: precisa receber a heap também
+           | Nulo                                            -- resultado de operações sem retorno ou valor nulo
+           | Erro String
+
+type Objeto = (Id, [(Id, Valor)])
 type Ambiente = [(Id,Valor)]
-type Estado = [(Id,Valor)]
--- int :: Ambiente -> [Termo] -> Estado -> (Valor, Estado)
+type Estado = [(Endereco, Objeto)]
+--int :: Ambiente -> Termo -> Estado -> (Valor, Estado)
