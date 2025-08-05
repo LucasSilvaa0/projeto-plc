27c8f03aa42b5f5233c59cd95226ed3643ddc362
Thomaz Cabral <127772709+thomazcabral@users.noreply.github.com>
2025-08-05 09:48:32 -0300

    Heap adicionada

--- projeto/projeto.hs
+++ projeto/projeto.hs
@@ -1,120 +1,153 @@
 type Id = String
 type Numero = Double
 
-data Definicao = Def Id Expressao -- antes tava como Termo, que não existe
-                 deriving (Show)
-
-data LValue = LVar Id                      -- L-Value de uma variável simples, ex: "x"
-            | LAttr Expressao Id           -- L-Value de um atributo, ex: "x" em "p.x"
-            | LThis
+data LValue = LVar Id 
+            | LAttr Expressao Id 
+            | LThis 
             deriving (Show)
 
--- sempre produzem um valor. pode ser um LValue
 data Expressao = Lit Numero
-               | Var LValue
-               | Som Expressao Expressao
-               | Mul Expressao Expressao
+                | Var LValue 
+                | Som Expressao Expressao 
+                | Mul Expressao Expressao 
+                | New Id [Expressao] 
+                | InstanceOf Expressao Id 
+                | Lam [Id] Expressao 
+                | CallMethod Expressao Id [Expressao] 
+                | Apl Expressao [Expressao] 
+                deriving (Show)
 
-               | New Id -- não altera o estado, mas altera a heap
-               | InstanceOf Expressao Id
+data Comando = Atr LValue Expressao 
+                | Seq Comando Comando 
+                | If Expressao Comando Comando 
+                | While Expressao Comando 
+                | For Comando Expressao Comando Comando 
+                | Classe Id [Id] [Definicao] 
+                | CmdExpr Expressao 
+                deriving (Show)
 
-               | Lam [Id] Expressao -- corpo de lambda deve ser uma expressão, não um comando
-               | CallMethod Expressao Id [Expressao]
-               | Apl Expressao [Expressao]
-               deriving (Show)
+data Definicao = Def Id Expressao 
+                deriving (Show)
 
--- muda o estado e controlam o fluxo
-data Comando = Atr LValue Expressao
-             | Seq Comando Comando
-             | If Expressao Comando Comando
-             | While Expressao Comando
-             | For Comando Expressao Comando Comando
-             | Classe Id [Id] [Definicao]
-             | CmdExpr Expressao
-             deriving (Show)
 
 type Endereco = Int
 
-data Valor = Num Numero                                      -- número
-           | BoolVal Bool                                    -- acho que precisa de um bool para if, while, etc.
-           | ObjectVal Endereco                              -- ponteiro
-           | Fun ([Valor] -> Estado -> (Valor, Estado))      -- função. TODO: precisa receber a heap também
-           | Nulo                                            -- resultado de operações sem retorno ou valor nulo
-           | Erro String
+data Valor = Num Numero 
+            | BoolVal Bool 
+            | ObjectVal Endereco 
+            | Fun ([Valor] -> Ambiente -> Estado -> Heap -> (Valor, Ambiente, Estado, Heap)) 
+            | Nulo 
+            | Erro String
 
 instance Show Valor where
-    show (Num n) = show n
-    show (BoolVal b) = show b
-    show (ObjectVal e) = "Object@" ++ show e
-    show (Fun _) = "<função>"
-    show Nulo = "null"
-    show (Erro msg) = "Erro: " ++ msg
+    show (Num n) = "Num " ++ show n
+    show (BoolVal b) = "Bool " ++ show b
+    show (ObjectVal e) = "ObjectRef(" ++ show e ++ ")"
+    show (Fun _) = "<function>"
+    show Nulo = "nulo"
+    show (Erro s) = "Erro: " ++ s
+
 
 type Objeto = (Id, [(Id, Valor)])
-type Ambiente = [(Id,Valor)]
-type Estado = [(Endereco, Objeto)]
+type Ambiente = [(Id, Valor)]
+type Estado = [(Id, Valor)]
+type Heap = [(Endereco, Objeto)]
 
-getValor :: Id -> Ambiente -> Valor -- busca o valor de uma variável no ambiente
-getValor id [] = Erro ("Variavel nao encontrada: " ++ id)
-getValor id ((idAtual, val) : resto)
-    | id == idAtual = val
-    | otherwise     = getValor id resto
+getVariavel :: Id -> Estado -> Valor
+getVariavel id est = case lookup id est of
+    Just val -> val
+    Nothing  -> Erro ("Variavel nao encontrada: " ++ id)
 
-setValor :: Id -> Valor -> Ambiente -> Ambiente -- -- atualiza ou adiciona um valor no ambiente
-setValor id val [] = [(id, val)]
-setValor id val ((idExistente, valExistente):resto) = if id == idExistente then (id, val) : resto else (idExistente, valExistente) : setValor id val resto
+setVariavel :: Id -> Valor -> Estado -> Estado
+setVariavel id val [] = [(id, val)]
+setVariavel id val ((idExistente, valExistente):resto) =
+    if id == idExistente
+    then (id, val) : resto
+    else (idExistente, valExistente) : setVariavel id val resto
 
-intExpressao :: (Maybe Endereco) -> Ambiente -> Estado -> Expressao -> (Valor, Ambiente, Estado)
-intExpressao ctx amb est expr = case expr of
-    (Lit n) -> (Num n, amb, est)
+getValor :: Id -> Ambiente -> Estado -> Valor
+getValor id amb est = case lookup id amb of
+    Just val -> val 
+    Nothing  -> getVariavel id est 
+
+intExpressao :: (Maybe Endereco) -> Ambiente -> Estado -> Heap -> Expressao -> (Valor, Ambiente, Estado, Heap)
+intComando   :: (Maybe Endereco) -> Ambiente -> Estado -> Heap -> Comando   -> (Ambiente, Estado, Heap)
+
+intExpressao ctx amb est heap expr = case expr of
+    (Lit n) -> (Num n, amb, est, heap)
 
     (Var lvalue) -> case lvalue of
-        (LVar id) -> (getValor id amb, amb, est)
-        LThis     -> error "This (em Var) ainda nao implementado"
-        _         -> error "Var de Atributo (p.x) ainda nao implementado"
+        (LVar id) -> (getValor id amb est, amb, est, heap)
+        _ -> error "Var para This ou Atributo nao implementado"
 
     (Som e1 e2) ->
-    let (v1, amb1, est1) = intExpressao ctx amb est e1 -- avalia a primeira expressão
-    in let (v2, amb2, est2) = intExpressao ctx amb1 est1 e2 -- avalia a segunda expressão no estado resultante da primeira
-        in case (v1, v2) of -- verifica se os resultados são números
-            (Num n1, Num n2) -> (Num (n1 + n2), amb2, est2) -- soma
-            _                -> (Erro "Erro de tipo: A operacao soma espera dois numeros", amb2, est2)
-            
-    (Mul e1 e2) -> error "Multiplicacao ainda nao implementada"
-    (New id) -> error "New ainda nao implementado"
-    (InstanceOf e id) -> error "InstanceOf ainda nao implementado"
-    (Lam ids corpo) -> error "Definicao de funcao ainda nao implementado"
-    (CallMethod objExpr nomeMetodo args) -> error "Chamada de metodo ainda nao implementado"
-    (Apl funcExpr args) -> error "Aplicacao de funcao ainda nao implementada"
+        let (v1, amb1, est1, heap1) = intExpressao ctx amb est heap e1
+        in let (v2, amb2, est2, heap2) = intExpressao ctx amb1 est1 heap1 e2
+            in case (v1, v2) of
+                (Num n1, Num n2) -> (Num (n1 + n2), amb2, est2, heap2)
+                _                -> (Erro "Erro de tipo: A operacao soma espera dois numeros.", amb2, est2, heap2)
 
-intComando :: (Maybe Endereco) -> Ambiente -> Estado -> Comando -> (Ambiente, Estado)
-intComando ctx amb est cmd = case cmd of
-    (Atr lvalue expr) -> case lvalue of
-        (LVar id) ->
-            let (val, ambAposExpr, estAposExpr) = intExpressao ctx amb est expr -- avalia a expressão da direita para pegar o valor
-            in (setValor id val ambAposExpr, estAposExpr) -- atualiza o ambiente com o novo valor para a variável e retorna o estado
-        _ -> error "Atribuicao de atributo ou This nao implementado."
+    (Apl (Var (LVar "menorQue")) [e1, e2]) -> -- Precisa implementar. Está impovisado
+        let (v1, amb1, est1, heap1) = intExpressao ctx amb est heap e1
+        in let (v2, amb2, est2, heap2) = intExpressao ctx amb1 est1 heap1 e2
+            in case (v1, v2) of
+                 (Num n1, Num n2) -> (BoolVal (n1 < n2), amb2, est2, heap2)
+                 _                -> (Erro "Argumentos invalidos para 'menorQue'.", amb2, est2, heap2)
+
+    (Mul e1 e2) -> error "Funcionalidade 'Multiplicacao' nao implementada."
+
+    (New id args) -> error "Funcionalidade 'New' nao implementada."
+
+    (InstanceOf e id) -> error "Funcionalidade 'InstanceOf' nao implementada."
+
+    (Lam ids corpo) -> error "Funcionalidade 'Definicao de Funcao' (Lam) nao implementada."
+
+    (CallMethod objExpr nomeMetodo args) -> error "Funcionalidade 'Chamada de Metodo' nao implementada."
+
+
+
+intComando ctx amb est heap cmd = case cmd of
+    (Atr (LVar id) expr) ->
+        let (val, ambAposExpr, estAposExpr, heapAposExpr) = intExpressao ctx amb est heap expr
+            novoEstado = setVariavel id val estAposExpr
+        in (ambAposExpr, novoEstado, heapAposExpr)
 
     (Seq c1 c2) ->
-        let (ambAposC1, estAposC1) = intComando ctx amb est c1 -- executa o primeiro comando
-        in intComando ctx ambAposC1 estAposC1 c2 -- executa o segundo comando no estado resultante do primeiro
+        let (amb1, est1, heap1) = intComando ctx amb est heap c1
+        in intComando ctx amb1 est1 heap1 c2
 
-    (If cond c1 c2) -> error "If ainda nao implementado"
-    (While cond corpo) -> intWhile ctx amb est (While cond corpo)
-    (For c1 e c2 c3) -> error "For ainda nao implementado"
-    (Classe id params defs) -> error "Declaracao de Classe ainda nao implementado"
-    (CmdExpr expr) -> error "Comando de Expressao ainda nao implementado"
+    (While cond corpo) -> intWhile ctx amb est heap (While cond corpo)
+    
+    (If cond c1 c2) -> error "Funcionalidade 'If' nao implementada."
 
-intWhile :: (Maybe Endereco) -> Ambiente -> Estado -> Comando -> (Ambiente, Estado)
-intWhile ctx amb est (While cond corpo) =
-    let (resultado, ambAposCond, estAposCond) = intExpressao ctx amb est cond -- avalia a expressão da condição no estado atual
-    in
-        case resultado of -- analisa o resultado da condição
-            (BoolVal True) -> -- se a condição for verdadeira
-                let (ambAposCorpo, estAposCorpo) = intComando ctx ambAposCond estAposCond corpo -- executa o corpo
-                in intWhile ctx ambAposCorpo estAposCorpo (While cond corpo) -- recursão
+    (For c1 e c2 c3) -> error "Funcionalidade 'For' nao implementada."
 
-            (BoolVal False) -> -- se for falsa
-                (ambAposCond, estAposCond) -- o while encerra e retorna o ambiente e estado atuais
+    (Classe id params defs) -> error "Funcionalidade 'Declaracao de Classe' nao implementada."
 
-            _ -> error ("Erro de Tipo: A condição do while deve ser um Booleano, mas foi: " ++ show resultado)
+    (CmdExpr expr) -> error "Funcionalidade 'Comando de Expressao' (CmdExpr) nao implementada."
+
+intWhile :: (Maybe Endereco) -> Ambiente -> Estado -> Heap -> Comando -> (Ambiente, Estado, Heap)
+intWhile ctx amb est heap (While cond corpo) =
+    let (resultado, amb1, est1, heap1) = intExpressao ctx amb est heap cond
+    in case resultado of
+        (BoolVal True) ->
+            let (amb2, est2, heap2) = intComando ctx amb1 est1 heap1 corpo
+            in intWhile ctx amb2 est2 heap2 (While cond corpo)
+        (BoolVal False) -> (amb1, est1, heap1)
+        _ -> error ("Erro de Tipo: A condição do 'while' deve ser um Booleano, mas foi: " ++ show resultado)
+
+
+-- i = 10
+testeAtr :: Comando
+testeAtr = Atr (LVar "i") (Lit 10)
+
+-- i = 10; j = i + 5
+testeSeq :: Comando
+testeSeq = Seq (Atr (LVar "i") (Lit 10))
+               (Atr (LVar "j") (Som (Var (LVar "i")) (Lit 5)))
+
+-- i = 0; while (i < 3) {i = i + 1}
+testeWhile :: Comando
+testeWhile = Seq (Atr (LVar "i") (Lit 0))
+                 (While (Apl (Var (LVar "menorQue")) [Var (LVar "i"), Lit 3])
+                        (Atr (LVar "i") (Som (Var (LVar "i")) (Lit 1))))
